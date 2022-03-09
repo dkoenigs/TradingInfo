6c6ce9c449738f4ed0041318260b8a318b96442c
Daniel Koenigsperger <Daniel.Koenigsperger@citrix.com>
2022-03-08 21:31:31 -0500

    Add project files.

---
+++ TradingInfo/Controllers/TradingInfoController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TradingInfo.Controllers
+{
+    [ApiController]
+    [Route("api/tradingInfo")]
+    public class TradingInfo : ControllerBase
+    {
+        private static readonly string[] StockTickers = new[]
+        {
+            "Walmart", "RocketLab NZ", "Amazon", "Meta"
+        };
+
+        private readonly ILogger<TradingInfo> _logger;
+
+        public TradingInfo(ILogger<TradingInfo> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public string GetDefault(int count)
+        {
+            return "Default";
+        }
+
+
+        [HttpGet]
+        [Route("dailyStockInfo")]
+        public IEnumerable<StockInfo> GetDailyStockInfo()
+        {
+            var rng = new Random();
+            List<StockInfo> dailyStockInfo = new List<StockInfo>();
+            foreach (string stock in StockTickers)
+            {
+                dailyStockInfo.Add(
+                    new StockInfo
+                    {
+                        StockTicker = stock,
+                        CompanyInfo = "company info",
+                        UnitPrice = rng.Next(100, 500),
+                        Date = DateTime.Now
+                    }
+                    );
+            }
+            return dailyStockInfo.ToArray();
+        }
+    }
+}
---
+++ TradingInfo/StockInfo.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TradingInfo
+{
+    public class StockInfo
+    {
+        public string StockTicker { get; set; }
+        public string CompanyInfo { get; set; }
+        public float UnitPrice { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
